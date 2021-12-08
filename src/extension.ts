7e978b38ffde4e5db3f2a9ddbcc6edd810c12ffe
Luke Lynch <Tech.Luk3@gmail.com>
2021-12-08 23:12:22 +1100

    adding command to context

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,12 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	
+    context.subscriptions.push(vscode.commands.registerCommand('extension.cucumberjs-debugger.getProgramName', config => {
+		return vscode.window.showInputBox({
+			placeHolder: "Please enter the name of a feature file in the workspace folder"
+		});
+	}));
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "cucumberjs-debugger" is now active!');
